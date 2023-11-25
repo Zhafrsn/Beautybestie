2604b9b89b7b3bf57f7975c181659a84554e4230
Zhafran <zhafranzaky.imel@gmail.com>
2023-11-25 22:14:49 +0800

    add product card

--- frontend/src/components/productcard/Productcard.tsx
+++ frontend/src/components/productcard/Productcard.tsx
@@ -1,5 +1,17 @@
-export const Productcard : React.FC = () => {
+import React from 'react';
+import '../../styles/Productcard.css'
+
+interface CardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+export const Productcard: React.FC<CardProps> = ({ title, description, imageUrl}) => {
   return (
-    <></>
+      <div className='card'>
+        <img src={imageUrl} alt={title} />
+        <h3>{title}</h3>
+      <p>{description}</p>
+      </div>
   );
 };
