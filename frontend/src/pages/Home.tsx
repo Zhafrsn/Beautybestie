1eabd70fe0ba61153a9304a1329024be504fc0e0
NameCard <sadinp21d@student.unhas.ac.id>
2023-11-25 17:53:26 +0800

    add pages home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,35 @@
+import Carousel from "components/Ads/Ads";
 import { MainLayout } from "layout";
+import '../styles/Home.css';
+import Card from '../components/Brand/Brand';
 
 export const Home: React.FC = () => {
+  const adImages = [
+    'images/image1.jpg',
+    'images/image2.jpg',
+    'images/image3.jpg',
+    'images/image4.jpg'
+  ];
+  const brandImage = [
+    { logo: 'images/logoSomethic.png' },
+    { logo: 'images/logoWardah.png' },
+    { logo: 'images/logoEmina.png' },
+    { logo: 'images/logoAzarine.png' },
+  ];
+
   return (
       <MainLayout>
-        <div>ini home</div>
-        <p>semua yang ada di sini adalah konten home</p>
+        <div className="Ads">
+          <Carousel images={adImages} />
+        </div>
+        <div className="ProductBrand">
+          <h1>PRODUCT BRANDS</h1>
+          <div className="Brand">
+            {brandImage.map((card, index) => (  
+            <Card key={index} logo={card.logo} /> 
+            ))}
+          </div>
+        </div>
       </MainLayout>
   );
 };
