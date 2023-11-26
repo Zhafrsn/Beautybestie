3a8ca48652d37cf65f33e872286c39b17e8313e2
Andimuhammadfauzanagung <andimuhammadfauzanagung@gmail.com>
2023-11-26 13:50:55 +0800

    adding bland ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/Global.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Products } from 'pages';
+import { Products } from './pages/Products';
 import { Cart } from './pages/Cart Pages/Cart';
 
 export const App: React.FC = () => {
