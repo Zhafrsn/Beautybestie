0802dff08a45fbe88d3b5d78f401309932ae4157
Leapz1 <bahrisyp@gmail.com>
2023-11-26 17:59:44 +0800

    adalah

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './styles/Global.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './components/Login/Login';
-import { Register } from './components/Register';
+import { Register } from './components/Register/Register';
 export const App: React.FC = () => {
   return (
     <>
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import '../../styles/Login.css';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 export const Login: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -12,22 +14,25 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <div className="negro">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h2 className="logen">Login</h2>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2 className="login-text">Login</h2>
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Login</button>
+      <section className="login-option-container">--- OR ---</section>
+      <div className="login-option-card">
+        <FontAwesomeIcon icon={faGoogle} className="sidebar__times-icon" />
+        <label className="option-text">login with google</label>
+      </div>
+    </form>
   );
 };
