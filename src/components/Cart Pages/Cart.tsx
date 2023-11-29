6d65b8c7675c0b12d6938d3273e8a935c3d8fe84
Andimuhammadfauzanagung <andimuhammadfauzanagung@gmail.com>
2023-11-29 20:18:36 +0800

    update

--- src/components/Cart Pages/Cart.tsx
+++ src/components/Cart Pages/Cart.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MainLayout } from 'layout';
 import '../../styles/Cart.css';
+import { useState } from 'react';
 export const Cart: React.FC = () => {
+  const [amount, setAmount] = useState('');
   return (
     <MainLayout>
       <div className="app">
@@ -10,18 +12,29 @@ export const Cart: React.FC = () => {
           <h2>Face Wash Bright stuff By Emna</h2>
           <p>Price : IDR.85.00</p>
           <p>Subtotal: IDR 85.000</p>
-          <button>Move To Whishlist</button>
+          <p>Move To Whishlist</p>
+          <section className="AmountButton1">
+            <button>+</button>
+            <input type="number" id="amount" name="amount" value={amount} onChange={e => setAmount(e.target.value)} />
+            <button>-</button>
+          </section>
         </div>
         <div className="product">
           <h2>Serum Bright stuff By Emna</h2>
           <p>Price : IDR.45.00</p>
           <p>Subtotal: IDR 45.000</p>
-          <button>Move To Whishlist</button>
+          <p>Move To Whishlist</p>
+          <section className="AmountButton1">
+            <button>+</button>
+            <input type="number" id="amount" name="amount" value={amount} onChange={e => setAmount(e.target.value)} />
+            <button>-</button>
+          </section>
         </div>
         <div className="summary">
           <h2>Summary</h2>
           <h3>Total Orders</h3>
-          <p>Buy IDR 130.000</p>
+          <p>IDR 130.000</p>
+          <button>Buy</button>
         </div>
       </div>
     </MainLayout>
