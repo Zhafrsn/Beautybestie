77ce1460e5c8f5ad62069ea3348e0465941f113f
NameCard <sadinp21d@student.unhas.ac.id>
2023-11-30 16:04:13 +0800

    update page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
-// import Carousel from "components/Ads/Ads";
+import Ads from "components/Ads/Ads";
 import { MainLayout } from "layout";
 import '../styles/Home.css';
-import Card from '../components/Brand/Brand';
+import Brand from '../components/Brand/Brand';
 
 export const Home: React.FC = () => {
-  // const adImages = [
-  //   'images/image1.jpg',
-  //   'images/image2.jpg',
-  //   'images/image3.jpg',
-  //   'images/image4.jpg'
-  // ];
+  const adImages = [
+    'images/image1.jpg',
+    'images/image2.jpg',
+    'images/image3.jpg',
+    'images/image4.jpg'
+  ];
   const brandImage = [
     { logo: 'images/logoSomethic.png' },
     { logo: 'images/logoWardah.png' },
@@ -19,14 +19,14 @@ export const Home: React.FC = () => {
 
   return (
       <MainLayout>
-        {/* <div className="Ads">
-          <Carousel images={adImages} />
-        </div> */}
+        <div className="Ads">
+          <Ads images={adImages} />
+        </div>
         <div className="ProductBrand">
           <h1>PRODUCT BRANDS</h1>
           <div className="Brand">
             {brandImage.map((card, index) => (  
-            <Card key={index} logo={card.logo} /> 
+            <Brand key={index} logo={card.logo} /> 
             ))}
           </div>
         </div>
