968a1b23e0a20e1b99af1bf65b4481bcfe77fa77
NameCard <sadinp21d@student.unhas.ac.id>
2023-11-30 21:35:56 +0800

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Ads from "components/Ads/Ads";
 import { MainLayout } from "layout";
 import '../styles/Home.css';
 import Brand from '../components/Brand/Brand';
+import { BestSeller } from '../components/Best Seller/BestSeller';
 
 export const Home: React.FC = () => {
   const adImages = [
@@ -17,11 +18,58 @@ export const Home: React.FC = () => {
     { logo: 'images/logoAzarine.png' },
   ];
 
+  const BestSellerData = [
+    {
+      title: 'Bright Stuff',
+      category: 'Face Wash',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash1.png',
+    },
+    {
+      title: 'Bright Stuff',
+      category: 'Face Wash',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash2.png',
+    },
+    {
+      title: 'Bright Stuff',
+      category: 'Face Wash',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash3.png',
+    },
+    {
+      title: 'Bright Stuff',
+      category: 'Face Wash',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash4.png',
+    },
+    {
+      title: 'Bright Stuff',
+      category: 'Face Wash',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash5.png',
+    },
+    {
+      title: 'Bright Stuff',
+      category: 'Face Wash',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash6.png',
+    },
+  ];
+
   return (
       <MainLayout>
         <div className="Ads">
           <Ads images={adImages} />
         </div>
+        <div className="BestSeller">
+          <h1>BEST SELLER</h1>
+        </div>
+        <div className="BestSellerCard">
+        {BestSellerData.map((card, index) => (
+          <BestSeller key={index} {...card} />
+        ))}
+      </div>
         <div className="ProductBrand">
           <h1>PRODUCT BRANDS</h1>
           <div className="Brand">
