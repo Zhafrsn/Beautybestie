3fa6cca1718e02fe71061b0ec8ecd2872812eb2d
Annisaraulitaa <annisaraulita240702@gmail.com>
2023-11-30 21:44:01 +0700

    improve wishlist page

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { MainLayout } from 'layout';
-import { EmptyStateWishlist } from '../components/Wishlist/EmptyStateWishlist';
+import { WishlistCard } from 'components/Wishlist/WishlistCard';
+// import { EmptyStateWishlist } from '../components/Wishlist/EmptyStateWishlist';
 
 
 const Wishlist: React.FC = () => {
   return (
     <MainLayout>
-        <EmptyStateWishlist/>
+      <section className="wishlist__kontainer">
+        <h1 className="wishlist__title">Wishlist</h1>
+        <hr/>
+      </section>
+       {/* <EmptyStateWishlist/> */}
+       <WishlistCard title={''} price={0} image={''} quantity={0} onIncreaseQuantity={function (): void {
+        throw new Error('Function not implemented.');
+      } } onDecreaseQuantity={function (): void {
+        throw new Error('Function not implemented.');
+      } }/>
     </MainLayout>
   );
 };
