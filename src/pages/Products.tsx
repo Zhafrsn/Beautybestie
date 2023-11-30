88f60b3a16764ae3de7fee3a8df1240ccd05b011
Zhafran <zhafranzaky.imel@gmail.com>
2023-11-30 21:00:45 +0800

    updated products

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -44,28 +44,27 @@ export const Products: React.FC = () => {
   ];
 
   const Option1 = ['All', 'Face Wash', 'Toner', 'Serum', 'Mask', 'Moisturizer', 'Sunscreen', 'Lip Care'];
+  const Option1 = ['All', 'Face Wash', 'Toner', 'Serum', 'Mask', 'Moisturizer', 'Sunscreen', 'Lip Care'];
   const Option2 = ['All', 'Lowest Price', 'Highest Price'];
   return (
     <MainLayout>
-      <div className='productsPage-container'>
-        <div className="Products-Judul">
-          <h1>Our Products</h1>
+      <div className="Products-Judul">
+        <h1>Our Products</h1>
+      </div>
+      <div className='Products-Dropdown'>
+        <div className='products-dd-category'>
+          <p>Category :</p>
+          <Dropdown options={Option1}/>
         </div>
-        <div className='Products-Dropdown'>
-          <div className='products-dd-category'>
-            <p>Category :</p>
-            <Dropdown options={Option1}/>
-          </div>
-          <div className='products-dd-sortby'>
-            <p>Sort By :</p>
-            <Dropdown options={Option2} />
-          </div>
+        <div className='products-dd-sortby'>
+          <p>Sort By :</p>
+          <Dropdown options={Option2} />
         </div>
-        <div className="Products-Card-Container">
-          {cardData.map((card, index) => (
-            <Productcard key={index} {...card} />
-          ))}
-        </div>
+      </div>
+      <div className="Products-Card-Container">
+        {cardData.map((card, index) => (
+          <Productcard key={index} {...card} />
+        ))}
       </div>
     </MainLayout>
   );
