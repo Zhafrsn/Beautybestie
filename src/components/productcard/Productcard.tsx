1675514097fd30c70c928511cf24bd0fa4cf4b74
Zhafran <zhafranzaky.imel@gmail.com>
2023-11-30 12:52:46 +0800

    update products page

--- src/components/productcard/Productcard.tsx
+++ src/components/productcard/Productcard.tsx
@@ -3,15 +3,15 @@ import '../../styles/Productcard.css'
 
 interface CardProps {
   title: string;
-  description: string;
+  Price: string;
   imageUrl: string;
 }
-export const Productcard: React.FC<CardProps> = ({ title, description, imageUrl}) => {
+export const Productcard: React.FC<CardProps> = ({ title, Price, imageUrl}) => {
   return (
       <div className='Products-Card'>
         <img src={imageUrl} alt={title} />
         <h3>{title}</h3>
-      <p>{description}</p>
+      <p>{Price}</p>
       </div>
   );
 };
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,39 +5,51 @@ import { Productcard } from '../components/productcard';
 export const Products: React.FC = () => {
   const cardData = [
     {
-      title: 'Card 1',
-      description: 'Description for Card 1',
-      imageUrl: 'https://example.com/card1.jpg',
+      title: 'Face Wash 1',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash1.png',
     },
     {
-      title: 'Card 2',
-      description: 'Description for Card 2',
-      imageUrl: 'https://example.com/card2.jpg',
+      title: 'Face Wash 2',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash2.png',
     },
     {
-      title: 'Card 3',
-      description: 'Description for Card 3',
-      imageUrl: 'https://example.com/card3.jpg',
+      title: 'Face Wash 3',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash3.png',
     },
     {
-      title: 'Card 4',
-      description: 'Description for Card 4',
-      imageUrl: 'https://example.com/card4.jpg',
+      title: 'Face Wash 4',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash4.png',
     },
     {
-      title: 'Card 5',
-      description: 'Description for Card 5',
-      imageUrl: 'https://example.com/card5.jpg',
+      title: 'Face Wash 5',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash5.png',
     },
     {
+<<<<<<< HEAD
       title: 'Card 6',
       description: 'Description for Card 6',
       imageUrl: 'https://example.com/card6.jpg',
+=======
+      title: 'Face Wash 6',
+      Price: 'IDR 19.000',
+      imageUrl: 'images/FaceWash6.png',
+>>>>>>> 4a0409d43ca4e322cd624c3ee7c5857e2dce112c
     },
   ];
   return (
     <MainLayout>
+<<<<<<< HEAD
       <h1 className="Products-Judul">Our Products</h1>
+=======
+      <div className="Products-Judul">
+      <h1>Our Products</h1>
+      </div>
+>>>>>>> 4a0409d43ca4e322cd624c3ee7c5857e2dce112c
       <div className="Products-Card-Container">
         {cardData.map((card, index) => (
           <Productcard key={index} {...card} />
