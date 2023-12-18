acec06d11960aa42da5905456791a5efae9f390c
misnaaaaws <misnawati240804@gmail.com>
2023-12-18 21:19:23 +0800

    feat: Connect database to page

--- src/pages/DetailProduct.tsx
+++ src/pages/DetailProduct.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MainLayout } from "layout";
-import { BestSeller } from "../components/Best Seller/BestSeller";
 import { UserReview } from "../components/User Review/UserReview";
 import "../styles/DetailProduct.css";
 import { ProductComp } from "components/Product Det/ProductComp";
@@ -8,8 +7,37 @@ import { faInstagram, faFacebook, faXTwitter } from '@fortawesome/free-brands-sv
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { RatingComp } from "components/RatingDetail/RatingComp";
+import { getAllProducts } from "../firebase/getAllProducts";
+import { TProduct } from "types/product.type";
+import { BestSeller } from "components/Best Seller";
+import { useParams } from "react-router-dom";
 
 export const DetailProduct: React.FC = () => {
+  const [Data, setData] = useState<TProduct[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [selectedProduct, setSelectedProduct] = useState<TProduct | null>(null);
+  const bestSellerProducts = Data.filter((product) => product.bestSeller === true);
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllProducts();
+        setData(data);
+        const product = data.find((product) => product.id === id);
+        setSelectedProduct(product || null);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data. Please try again later.');
+        setLoading(false);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
   const UserReviewData = [
     {
       rating: "5.0",
@@ -40,68 +68,45 @@ export const DetailProduct: React.FC = () => {
       Image: 'images/FaceWash4.png',
     }
   ];
-  const ProductRecData = [
-    {
-      title: 'Bright Stuff',
-      category: 'Face Wash',
-      Price: 'IDR 19.000',
-      imageUrl: 'images/FaceWash1.png',
-    },
-    {
-      title: 'Bright Stuff',
-      category: 'Face Wash',
-      Price: 'IDR 19.000',
-      imageUrl: 'images/FaceWash2.png',
-    },
-    {
-      title: 'Bright Stuff',
-      category: 'Face Wash',
-      Price: 'IDR 19.000',
-      imageUrl: 'images/FaceWash3.png',
-    },
-    {
-      title: 'Bright Stuff',
-      category: 'Face Wash',
-      Price: 'IDR 19.000',
-      imageUrl: 'images/FaceWash4.png',
-    },
-    {
-      title: 'Bright Stuff',
-      category: 'Face Wash',
-      Price: 'IDR 19.000',
-      imageUrl: 'images/FaceWash5.png',
-    },
-    {
-      title: 'Bright Stuff',
-      category: 'Face Wash',
-      Price: 'IDR 19.000',
-      imageUrl: 'images/FaceWash6.png',
-    },
-  ];
+
+  if (loading) {
+    return (
+      <div className='loading-spinner-container'>
+        <div className='loading-spinner'></div>
+      </div>
+    ); 
+  }
+
+  if (error) {
+    return <div className='error-message'>{error}</div>; 
+  }
+
+  if (!selectedProduct) {
+    return <div className='error-message'>Product not found</div>;
+  }
+
+  const { image, description } = selectedProduct;
 
   return (
     <MainLayout>
       <div className="Pro_Comp">
         <div className="card_left">
           <div className="big_card">
-            <img src="images/FaceWash1.png" alt="image1" />
+            <img src={image} alt="image1" className="big_card-img"/>
           </div>
           <div className="small_card">
-            <img className="small_card1" src="images/FaceWash1.png" alt="image2" />
-            <img className="small_card2" src="images/FaceWash1.png" alt="image3" />
-            <img className= "small_card3" src="images/FaceWash1.png" alt="image4" />
+            <img className="small_card1" src={image} alt="image2" />
+            <img className="small_card1" src={image} alt="image3" />
+            <img className= "small_card1" src={image} alt="image4" />
           </div>
         </div>
         <div className="card_right">
-          <ProductComp />
+          <ProductComp {...selectedProduct} />
         </div>
       </div>
       <div className='desc_product'>
         <h2>Description:</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quisquam harum corrupti
-          dolorum nam saepe expedita fugit eius repellendus laudantium dicta reiciendis rerum fugiat
-          nobis velit quidem veniam porro obcaecati, recusandae sapiente, illo ut iure modi nemo! Voluptates
-          nam blanditiis quod asperiores culpa ipsum illo incidunt aliquam modi hic recusandae eligendi</p>
+        <p>{description}</p>
         <div className='logo_desc'>
           <FontAwesomeIcon icon={faFacebook} style={{ color: "#005eff", }} />
           <FontAwesomeIcon icon={faXTwitter} style={{color: "#000000",}} />
@@ -125,8 +130,8 @@ export const DetailProduct: React.FC = () => {
       <div className="Pro_Rec">
         <h1>Product Recommendation</h1>
         <div className="Pro_Rec_Card">
-          {ProductRecData.map((card, index) => (
-            <BestSeller key={index} {...card} />
+          {bestSellerProducts.map((product, index) => (
+            <BestSeller key={index} {...product} />
           ))}
         </div>
       </div>
